269aff4d77eeb11c4c7efe0b482e9ee96367fe9b
Yi <mavisluan83@gmail.com>
2018-12-09 11:40:51 -0800

    Add tests for GET /todos

--- server/server.js
+++ server/server.js
@@ -1,18 +1,13 @@
-// load library imports
 const express = require('express')
 const bodyParser = require('body-parser')
-// load local imports
 const { mongoose } = require('./db/mongoose')
 const { Todo } = require('./models/todo')
 const { User } = require('./models/user')
 
 const app = express()
 
-// use express middleware
 app.use(bodyParser.json())
-// create Post route to create new todos
 app.post('/todos', (req, res) => {
-    // console.log(req.body)
     const todo = new Todo({
         text: req.body.text
     })
