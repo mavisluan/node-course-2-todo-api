37a4363666eb857e87090d780cd896843882a786
Yi <mavisluan83@gmail.com>
2018-12-13 21:50:36 -0800

    Add POST /users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,58 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
+const JWT = require('jsonwebtoken')
+const _ = require('lodash')
 
-const User = mongoose.model('User', {
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 1
-    }
+const UserSchema = new mongoose.Schema({
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+            minlength: 1,
+            unique: true,
+            validate: {
+                validator: validator.isEmail,
+                message: `{VALUE} is not a valid email`
+            }
+        },
+        password: {
+            type: String,
+            require: true,
+            minlength: 6
+        },
+        tokens: [{
+            access: {
+                type: String,
+                required: true
+            },
+            token: {
+                type: String,
+                required: true
+            }
+        }]
 })
 
+UserSchema.methods.toJSON = function () {
+    const user = this
+    const userObject = user.toObject()
+    
+    return _.pick(userObject, ['_id', 'email'])
+}
+
+UserSchema.methods.generateAuthToken = function () {
+    const user = this
+    const access = 'auth'
+    const token = JWT.sign({_id: user._id.toHexString(), access }, 'abc123').toString()
+//  push the token into tokens array
+    user.tokens = user.tokens.concat([{ access, token }])
+//  save the user into db 
+//  when you return two chain in a promise, you return another promise. We're returning a value, which will get passed as the success argument for the next then call. (server.js)
+    return user.save().then(() => {
+        return token
+    })
+}
+
+const User = mongoose.model('User', UserSchema)
+
 module.exports = { User }
+
